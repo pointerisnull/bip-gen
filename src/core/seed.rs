f8af58b1586879f57ddc5f3a14da86830879704a
Bren <92599085+pointerisnull@users.noreply.github.com>
2026-04-21 09:24:39 -0400

    Wallet seed structure

--- src/core/seed.rs
+++ src/core/seed.rs
@@ -1,1 +1,34 @@
+#[allow(dead_code)]
 
+#[derive(Debug)]
+pub struct BIPSeed {
+    words: Vec<u16>,
+    word_count: u8,
+}
+
+impl BIPSeed {
+
+    pub fn new(word_count: u8) -> BIPSeed {
+        BIPSeed {
+            words: Vec::new(),
+            word_count: word_count,
+        }
+    }
+
+    pub fn add_word(&mut self, val: u16) {
+
+        self.words.push(val);
+
+    }
+
+    pub fn get_word(&mut self, index: u8) -> u16 {
+
+        if (index as usize) >= self.words.len() {
+            return 0xFFFF;
+        }
+
+        return self.words[index as usize];
+
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod rand;
+mod core;
 use rand::twister;
+use core::seed;
 
 fn main() {
     // Seed rng with /udev/rand or with user input (NOT SECURE!)
     twister::twist(32);
     // Generate word list (integer index, 0->2047)
+    let mut bipseed: seed::BIPSeed = seed::BIPSeed::new(12);
 
+    bipseed.add_word(23);
+    bipseed.add_word(0x152);
+    bipseed.add_word(0xFF);
 
+    println!("{:?}", bipseed);
 }
